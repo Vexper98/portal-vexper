4451efb3d7db5811fe636747710f23ce09df324b
Cotaplus <cotaplusoficial@gmail.com>
2026-03-17 12:16:37 +0000

    File changes

--- src/components/dashboard/RecentDocumentsTable.jsx
+++ src/components/dashboard/RecentDocumentsTable.jsx
@@ -90,7 +90,7 @@ export default function RecentDocumentsTable({ documents }) {
                     </a>
                   )}
                 </td>
-              </motion.tr>
+              </tr>
             );
           })}
         </tbody>
